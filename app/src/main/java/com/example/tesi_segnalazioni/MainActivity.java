660b7eed8a1fe12eb2ed2a3bdafcb133b4b30e3f
silvialorusso <silvia.lorusso90@gmail.com>
2019-12-09 18:50:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tesi_segnalazioni/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tesi_segnalazioni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
